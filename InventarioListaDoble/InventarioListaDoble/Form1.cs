b82380524e24c8ebb0cf4a8534d615e92a4834d4
Mildred Silva M <mildred_natali@hotmail.com>
2017-05-07 23:39:26 -0500

    ListaDoble

---
+++ InventarioListaDoble/InventarioListaDoble/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InventarioListaDoble
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Lista l = new Lista();
+        int codigo;
+        string nombre;
+        int cantidad;
+        int precio;
+        Nodo n1;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            codigo = Convert.ToInt32(textBox1.Text);
+            nombre = textBox2.Text;
+            cantidad = Convert.ToInt32(textBox3.Text);
+            precio = Convert.ToInt32(textBox4.Text);
+            n1 = new Nodo(codigo, nombre, cantidad, precio);
+            l.agregar(n1);
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            codigo = Convert.ToInt32(textBox1.Text);
+            l.eliminar(codigo);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int codigo = Convert.ToInt32(textBox1.Text);
+            string nombre = textBox2.Text;
+            int cantidad = Convert.ToInt32(textBox3.Text);
+            int precio = Convert.ToInt32(textBox4.Text);
+            int posicion = int.Parse(textBox6.Text);
+
+            l.insertar(new Nodo(codigo, nombre, cantidad, precio), posicion);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Nodo n1;
+            int b = Convert.ToInt32(textBox6.Text);
+            n1 = l.buscar(b);
+            if (n1 != null)
+            {
+                textBox5.Text = n1.ToString();
+            }
+
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox5.Clear();
+            textBox5.Text += l.lista();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox5.Clear();
+            textBox5.Text += l.reporteI();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            l.eliminarPrimero();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            l.eliminarUltimo();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            codigo = Convert.ToInt32(textBox1.Text);
+            nombre = textBox2.Text;
+            cantidad = Convert.ToInt32(textBox3.Text);
+            precio = Convert.ToInt32(textBox4.Text);
+            n1 = new Nodo(codigo, nombre, cantidad, precio);
+            l.agregarInicio(n1);
+        }
+    }
+}
---
+++ InventarioListaDoble/InventarioListaDoble/Lista.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventarioListaDoble
+{
+    class Lista
+    {
+        public Nodo primero = null;
+        public Nodo ultimo = null;
+    
+
+        public Lista()
+        {
+
+        }
+        //AGREGAR
+        public void agregar(Nodo nodo)
+        {
+            if (primero == null)
+                primero = nodo;
+            else
+                agregar(primero, nodo);
+        }
+
+        private void agregar(Nodo ultimo, Nodo nodo)
+        {
+            if (ultimo.siguiente == null)
+                ultimo.siguiente = nodo;
+            else
+                agregar(ultimo.siguiente, nodo);
+
+        }
+        //AGREGAR INICIO
+        public void agregarInicio(Nodo nuevoNodo)
+        {
+            if (primero == null)
+            {
+                primero = nuevoNodo;
+               
+            }
+            else
+            {
+                nuevoNodo.siguiente = primero;
+                primero.anterior = nuevoNodo;
+                primero = nuevoNodo;
+            }
+        }
+        //BUSQUEDA
+        public Nodo buscar(int codigo)
+        {
+            Nodo search = primero;
+            while (search != null)
+            {
+                if (search.Codigo == codigo)
+                {
+                    return search;
+                }
+                else
+                {
+                    search = search.siguiente;
+                }
+            }
+            return null;
+        }
+        //ELIMINAR
+        public void eliminar(int codigo)
+        {
+            if (primero.Codigo == codigo)
+            {
+                primero = primero.siguiente;
+
+            }
+            else
+            {
+                Nodo temporal = primero;
+
+                while (temporal.siguiente != null)
+                {
+                    if (temporal.siguiente.Codigo == codigo)
+                    {
+                        temporal.siguiente = temporal.siguiente.siguiente;
+
+                        break;
+                    }
+                    else
+                    {
+                        temporal = temporal.siguiente;
+                    }
+                }
+            }
+
+        }
+        //ELIMINAR PRIMERO
+        public string eliminarPrimero()
+        {
+            if (primero != null)
+            {
+                primero = primero.siguiente;
+                primero.anterior = null;
+                return " ";
+            }
+            else
+                return null;
+        }
+        //ELIMINAR ULTIMO
+        public string eliminarUltimo()
+        {
+            
+            if (ultimo != null)
+            {
+                ultimo = ultimo.anterior;
+                ultimo.siguiente = null;
+                return " ";
+            }
+            else
+                return null;
+        }
+        //INSERTAR
+        public void insertar(Nodo producto, int posicion)
+        {
+            {
+                if ((primero != null) && (posicion > 0))
+                {
+                    Nodo temp = primero;
+                    int inicio = 1;
+                    while (true)
+                    {
+                        if (inicio <= posicion)
+                        {
+                            producto.siguiente = temp.siguiente;
+                            temp.siguiente = producto;
+                            break;
+                        }
+                        else
+                        {
+                            inicio++;
+                            temp = temp.siguiente;
+                        }
+                    }
+                }
+
+            }
+        }
+        //REPORTE INVERSO
+        public string reporteI()
+        {
+            if (primero == null)
+                return "";
+            else
+                return reporteI(primero);
+
+        }
+        public string reporteI(Nodo nodo)
+        {
+            if (nodo.siguiente == null)
+                return nodo.ToString() + Environment.NewLine;
+            else
+                return reporteI(nodo.siguiente) + nodo.ToString() + Environment.NewLine;
+        }
+        //LISTA
+        public string lista()
+        {
+            string show = ToString() + Environment.NewLine;
+            Nodo temporal = primero;
+            while (temporal != null)
+            {
+                show += " " + Environment.NewLine;
+                show += temporal.ToString();
+                temporal = temporal.siguiente;
+            }
+            return show;
+        }
+
+    }
+}
+
